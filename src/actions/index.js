37ac6001267267e9fea8520cf10f375e3394b09a
Tara Ragsdale <Taradacktl@aol.com>
2018-10-05 21:41:24 -0700

    actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,18 @@
-export const SET_PRINCIPAL = 'SET_PRINCIPAL';
-export const setPrincipal = principal => ({
-    type: SET_PRINCIPAL,
-    principal
+export const RESTART_GAME = 'RESTART_GAME';
+export const restartGame = correctAnswer => ({
+    type: RESTART_GAME,
+    correctAnswer
 });
 
 
-export const SET_INTEREST = 'SET_INTEREST';
-export const setInterest = interest => ({
-    type: SET_INTEREST,
-    interest
+export const MAKE_GUESS = 'MAKE_GUESS';
+export const makeGuess = guess => ({
+    type: MAKE_GUESS,
+    guess
 });
 
 
-export const SET_YEARS = 'SET_YEARS';
-export const setYears = years => ({
-    type: SET_YEARS,
-    years
+export const GENERATE_AURAL_UPDATE = 'GENERATE_AURAL_UPDATE';
+export const generateAuralUpdate = () => ({
+    type: GENERATE_AURAL_UPDATE
 });
